fd1ed7ead253081652d04cf55afe43098fa8fed1
Paulo Olivo <paulo.olivo@udla.edu.ec>
2024-12-08 22:41:05 -0500

    Agregar archivos de proyecto.

---
+++ Models/Catalogo.cs
@@ -0,0 +1,86 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace SPJ_ProyectoMVC.Models
+{
+    public class Catalogo
+    {
+        public int CatalogoId { get; set; }
+
+        [Required]
+        public string? Marca { get; set; }
+        
+        public string? Modelo { get; set; }
+        
+        public bool Usado { get; set; }
+        [Range(5000.00, 50000.00)]
+        public decimal Precio { get; set; }
+        public decimal IVA { get; set; }
+        public string? ImagePath { get; set; }
+
+    }
+
+
+public static class CatalogoEndpoints
+{
+	public static void MapCatalogoEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/Catalogo").WithTags(nameof(Catalogo));
+
+        group.MapGet("/", async (SPJwebAPIContext db) =>
+        {
+            return await db.Catalogo.ToListAsync();
+        })
+        .WithName("GetAllCatalogos")
+        .WithOpenApi();
+
+        group.MapGet("/{id}", async Task<Results<Ok<Catalogo>, NotFound>> (int catalogoid, SPJwebAPIContext db) =>
+        {
+            return await db.Catalogo.AsNoTracking()
+                .FirstOrDefaultAsync(model => model.CatalogoId == catalogoid)
+                is Catalogo model
+                    ? TypedResults.Ok(model)
+                    : TypedResults.NotFound();
+        })
+        .WithName("GetCatalogoById")
+        .WithOpenApi();
+
+        group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int catalogoid, Catalogo catalogo, SPJwebAPIContext db) =>
+        {
+            var affected = await db.Catalogo
+                .Where(model => model.CatalogoId == catalogoid)
+                .ExecuteUpdateAsync(setters => setters
+                  .SetProperty(m => m.CatalogoId, catalogo.CatalogoId)
+                  .SetProperty(m => m.Marca, catalogo.Marca)
+                  .SetProperty(m => m.Modelo, catalogo.Modelo)
+                  .SetProperty(m => m.Usado, catalogo.Usado)
+                  .SetProperty(m => m.Precio, catalogo.Precio)
+                  .SetProperty(m => m.IVA, catalogo.IVA)
+                  .SetProperty(m => m.ImagePath, catalogo.ImagePath)
+                  );
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("UpdateCatalogo")
+        .WithOpenApi();
+
+        group.MapPost("/", async (Catalogo catalogo, SPJwebAPIContext db) =>
+        {
+            db.Catalogo.Add(catalogo);
+            await db.SaveChangesAsync();
+            return TypedResults.Created($"/api/Catalogo/{catalogo.CatalogoId}",catalogo);
+        })
+        .WithName("CreateCatalogo")
+        .WithOpenApi();
+
+        group.MapDelete("/{id}", async Task<Results<Ok, NotFound>> (int catalogoid, SPJwebAPIContext db) =>
+        {
+            var affected = await db.Catalogo
+                .Where(model => model.CatalogoId == catalogoid)
+                .ExecuteDeleteAsync();
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("DeleteCatalogo")
+        .WithOpenApi();
+    }
+}}
---
+++ Models/SPJwebAPIContext.cs
@@ -0,0 +1,7 @@
+﻿namespace SPJ_ProyectoMVC.Models
+{
+    internal class SPJwebAPIContext
+    {
+        public object Catalogo { get; internal set; }
+    }
+}
